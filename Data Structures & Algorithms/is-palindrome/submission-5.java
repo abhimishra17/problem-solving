4120a089e55a4b94aca388187f41219891629979
Abhishek Kumar Mishra <153258500+abhimishra17@users.noreply.github.com>
2026-07-06 23:40:09 +0530

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String str=s.replaceAll("[^A-Za-z0-9]","").toLowerCase();
+        int i=0;
+        int j=str.length()-1;
+        while(i<j){
+            if(str.charAt(i)!=str.charAt(j)){
+                System.out.println(str.charAt(i)+" "+str.charAt(j));
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+      return true;  
+    }
+}
